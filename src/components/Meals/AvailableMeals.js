22de0ba345ec293e8fc8f6d8e3d15cc94741532a
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-12-29 12:09:34 +1100

    adding a checkout form

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -10,7 +10,7 @@ const AvailableMeals = () => {
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        "https://react-http-fcdd7-default-rtdb.firebaseio.com/meals"
+        "https://react-http-fcdd7-default-rtdb.firebaseio.com/meals.json"
       );
       if (!response.ok) {
         throw new Error("Something went wrong");
